2c97d1997ce8d1f20ba3d3e3f4004725e230e1b8
qwang <wangqianyu231@gmail.com>
2015-11-06 17:49:15 -0600

    Create MVC framework using maven

---
+++ chineseAirportService/src/test/java/junitTest/JunitTest.java
@@ -0,0 +1,12 @@
+package junitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JunitTest {
+	@Test
+	public void shouldReturnTrue(){
+		assertTrue(1==1);
+	}
+}
---
+++ chineseAirportService/src/test/java/springTest/SpringFrameworkTest.java
@@ -0,0 +1,5 @@
+package springTest;
+
+public class SpringFrameworkTest {
+
+}
